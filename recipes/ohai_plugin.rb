c31f610dd0cbdc53e557d941ec8ecbe62bdb93fd
Tom Noonan II <thomas.noonan@rackspace.com>
2014-02-04 06:50:08 -0600

    Working plugin

--- recipes/ohai_plugin.rb
+++ recipes/ohai_plugin.rb
@@ -1,3 +1,20 @@
+# Cookbook Name:: rackspace_cloudbackup
+#
+# Copyright 2014, Rackspace, US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 template "#{node['ohai']['plugin_path']}/rackspace_cloudbackup.rb" do
   source 'plugins/rackspace_cloudbackup.rb.erb'
   owner  'root'
@@ -16,9 +33,7 @@ include_recipe 'ohai::default'
 
 ruby_block 'print cloudbackup info' do
   block do
-    Chef::Log.warn("DEBUG: #{node['rackspace']}")
-    Chef::Log.warn("DEBUG: #{node['rcbu']}")
-#    Chef::Log.warn("DEBUG: Registered: #{node['rackspace']['cloudbackup']['is_registered']}")
-#    Chef::Log.warn("DEBUG: Agent ID: #{node['rackspace']['cloudbackup']['agent_id']}")
+    Chef::Log.info("Rackspace CloudBackup Agent Registered: #{node['rcbu']['is_registered']}")
+    Chef::Log.info("Rackspace CloudBackup Agent Agent ID: #{node['rcbu']['agent_id']}")
   end
 end
