1ea38627f00566f9386cab06ac44db7e060cb76d
Tom Noonan II <thomas.noonan@rackspace.com>
2014-02-04 06:32:26 -0600

    Working rcbu namespace

--- recipes/ohai_plugin.rb
+++ recipes/ohai_plugin.rb
@@ -1,10 +1,10 @@
-plugin_install = template "#{node['ohai']['plugin_path']}/rackspace_cloudbackup.rb" do
+template "#{node['ohai']['plugin_path']}/rackspace_cloudbackup.rb" do
   source 'plugins/rackspace_cloudbackup.rb.erb'
   owner  'root'
   group  'root'
   mode   '0644'
   variables(bootstrap_file: '/etc/driveclient/bootstrap.json')
-  #  notifies :reload, 'ohai[reload_rackspace_cloudbackup]', :immediately
+  notifies :reload, 'ohai[reload]', :immediately
   action :create
 end
 
@@ -12,10 +12,13 @@ ohai "reload" do
   action :reload
 end
 
+include_recipe 'ohai::default'
+
 ruby_block 'print cloudbackup info' do
   block do
     Chef::Log.warn("DEBUG: #{node['rackspace']}")
-    #Chef::Log.warn("DEBUG: Registered: #{node['rackspace']['cloudbackup']['is_registered']}")
-    #Chef::Log.warn("DEBUG: Agent ID: #{node['rackspace']['cloudbackup']['agent_id']}")
+    Chef::Log.warn("DEBUG: #{node['rcbu']}")
+#    Chef::Log.warn("DEBUG: Registered: #{node['rackspace']['cloudbackup']['is_registered']}")
+#    Chef::Log.warn("DEBUG: Agent ID: #{node['rackspace']['cloudbackup']['agent_id']}")
   end
 end
