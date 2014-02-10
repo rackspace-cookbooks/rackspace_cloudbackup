9701b2c09325aee8d257bd551b35c5ea25df0d22
Tom Noonan II <thomas.noonan@rackspace.com>
2014-02-10 04:34:30 -0600

    Define new config structure

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,19 +16,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default[:rackspace_cloudbackup][:rackspace_username] = nil
-default[:rackspace_cloudbackup][:rackspace_apikey] = nil
-default[:rackspace_cloudbackup][:rackspace_endpoint] = "dfw"
-default[:rackspace_cloudbackup][:backup_locations] = nil
-default[:rackspace_cloudbackup][:backup_container] = nil
-default[:rackspace_cloudbackup][:backup_cron_day] = "*"
-default[:rackspace_cloudbackup][:backup_cron_hour] = "3"
-default[:rackspace_cloudbackup][:backup_cron_minute] = "14"
-default[:rackspace_cloudbackup][:backup_cron_month] = "*"
-default[:rackspace_cloudbackup][:backup_cron_weekday] = "*"
-default[:rackspace_cloudbackup][:backup_cron_user] = nil
-default[:rackspace_cloudbackup][:backup_cron_mailto] = nil
-default[:rackspace_cloudbackup][:backup_cron_path] = nil
-default[:rackspace_cloudbackup][:backup_cron_shell] = nil
-default[:rackspace_cloudbackup][:backup_cron_home] = nil
-default[:rackspace_cloudbackup][:cloud_notify_email] = nil
+#
+# General Settings
+#
+
+# We use the shared node['rackspace']['cloud_credentials'] for the username and api key
+# These credentials are required.
+# default['rackspace']['cloud_credentials']['username']
+# default['rackspace']['cloud_credentials']['api_key']
+
+# Datacenter to send backups to
+# TODO: automate setting of this
+default['rackspace']['datacenter'] = 'DFW'
+
+#
+# Backups configuration
+#
+
+# backups is a list of hashes of filesystem locations to backup
+# The hash format is: {
+#   location: filesystem path to backup (Required)
+#   cloud_notify_email: Email addresses for notifications on Rackspace Cloud (Rackspace-Cloud only)
+#   container: Cloud Files container to back up to (Non-Rackspace cloud only)
+#   comment:   Some comment (optional)
+#   enable:    Enable the backup, Boolean, Optional with default of true
+#   time: Time override hash for this backup.  (Optional) Format: {
+#      day: Day of month to run backup
+#      month: Month to run backup
+#      hour: Hour to run backup
+#      minute: Minute to run backup
+#      weekday: Day of week to run backup
+#   }
+#   cron: Cron override hash for this backup.  (Optional) Format: {
+#      user:   User to run the job as
+#      mailto: Address to send error messages to
+#      path:   Cron path
+#      shell:  Cron shell
+#      home:   Cron home
+#  }  
+# }
+# Many of the above options will pull from the default hash.
+# See the default hash description below for defaults and requirement details
+default['rackspace_cloudbackup']['backups'] = []
+
+#
+# Defaults
+#
+
+# backups_defaults: hash of default attributes for the backups
+# container: Backup target container for backups not on Rackspace public cloud
+#    Required on non-public cloud servers
+default['rackspace_cloudbackup']['backups_defaults']['container'] = nil
+
+# cloud_notify_email: Email address to send notifications from Rackspace Cloud Backups to
+#   Required on Rackspace Cloud.  Notifications will come from Rackspace servers, must be a valid address.
+# Note: This is different from the Cron email address in case there are any issues with mail from the system MTA
+default['rackspace_cloudbackup']['backups_defaults']['cloud_notify_email'] = nil
+
+# time: backup timing settings.  These settings are Cron format.
+default['rackspace_cloudbackup']['backups_defaults']['time']['day']     = "*"
+default['rackspace_cloudbackup']['backups_defaults']['time']['month']   = "*"
+default['rackspace_cloudbackup']['backups_defaults']['time']['hour']    = "3"
+default['rackspace_cloudbackup']['backups_defaults']['time']['minute']  = "14"
+default['rackspace_cloudbackup']['backups_defaults']['time']['weekday'] = "*"
+
+# Cron settings: Default settings for the cron jobs
+default['rackspace_cloudbackup']['backups_defaults']['cron']['user']   = nil
+default['rackspace_cloudbackup']['backups_defaults']['cron']['mailto'] = nil
+default['rackspace_cloudbackup']['backups_defaults']['cron']['path']   = nil
+default['rackspace_cloudbackup']['backups_defaults']['cron']['shell']  = nil
+default['rackspace_cloudbackup']['backups_defaults']['cron']['home']   = nil
