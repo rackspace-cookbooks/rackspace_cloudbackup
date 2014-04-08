f01d7ef68b27002bb6a8e66231ffa1b420fb9923
Tom Noonan II <thomas.noonan@rackspace.com>
2014-04-08 17:28:03 -0500

    Update metadata and Berksfile with new rackspace_ conventions

--- Berksfile
+++ Berksfile
@@ -1,4 +1,3 @@
-site :opscode
-
 metadata
-cookbook "yum", "~> 2.0"
+cookbook "rackspace_yum",  "~> 4.0", git: "https://github.com/rackspace-cookbooks/rackspace_yum"
+cookbook "rackspace_apt", "~> 3.0", git: "https://github.com/rackspace-cookbooks/rackspace_apt"
--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,14 @@
-name             'rackspace-cloud-backup'
+name             'rackspace_cloudbackup'
 maintainer       'Rackspace US, Inc.'
-maintainer_email 'matt.thode@rackspace.com'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures rackspace-cloud-backup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version          '1.0.0'
 
 recipe           "rackspace-cloud-backup", "Installs and registers cloud backup"
 
-depends "apt"
-depends "yum", "~> 2.0"
+depends "rackspace_apt", "~> 3.0"
+depends "rackspace_yum", "~> 4.0"
 depends "ohai"
