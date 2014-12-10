dbac0c7cdf2e089924e24b8f8dd74b181f0d4feb
Steve Jansen <stevejansen_github@icloud.com>
2014-12-10 01:19:23 -0500

    README update

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version          '1.0.1'
 
-recipe           'rackspace-cloud-backup', 'Installs and registers cloud backup'
+recipe           'default', 'Installs and registers cloud backup'
+recipe           'cloud', 'Installs and registers cloud backup, bypasses cloud provider check'
+recipe           'cloud_agent', 'Registers the node as a cloud backup system'
 
 depends 'apt', '~> 2.0'
 depends 'yum', '~> 3.0'
